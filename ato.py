cc112bd258db928272146d2d4117e09a7b9b634d
Conrad Heidebrecht <cheidebrecht@espial.com>
2018-02-05 12:47:37 -0500

    added deps

--- ato.py
+++ ato.py
@@ -77,10 +77,11 @@ if __name__ == '__main__':
     for pack in packages:
         v_logger(h.LOG_MODE.INFO, 'Parsing {} {}..'.format(pack, PACKAGE_EXTENSION.strip('.').upper()))
         package = h.parse_json(PACKAGE_DIR + pack + PACKAGE_EXTENSION)
-        manager, install_cmd, items, dry_cmd = (package['package_manager'],
-                                                package['install_cmd'],
-                                                package['packages'],
-                                                package['dry_run'] if 'dry_run' in file_dict.keys() else '')
+        manager, install_cmd, fix_cmd, items, dry_cmd = (package['package_manager'],
+                                                         package['install_cmd'],
+                                                         package['fix_cmd'] if 'fix_cmd' in package.keys() else None,
+                                                         package['packages'],
+                                                         package['dry_run'] if 'dry_run' in file_dict.keys() else '')
         # execute prerequesites for package installs
         for item in items:
             if "prereq" in item.keys():
@@ -93,10 +94,22 @@ if __name__ == '__main__':
                                    run=lambda: Popen(prereq_params).wait()), 0):
                         v_logger(h.LOG_MODE.ERR, '{}: Failed to execute prerequesites of {}'.format(pack, item['name']))
                         raise Exception('Failed to execute prerequesites of {} from package {}'.format(item['name'], pack))
+            if "deps" in item.keys():
+                v_logger(h.LOG_MODE.INFO, '{}: Installing dependencies for {}..'.format(pack, item['name']))
+                deps_params = [install_cmd, dry_cmd if DRY_RUN else ''] + [dep for dep in item['deps']]
+                if not c_logger(Sn(stringify=lambda: ' '.join(deps_params),
+                               run=lambda: Popen(deps_params).wait()), 0):
+                    v_logger(h.LOG_MODE.ERR, '{}: Failed to install dependencies for {}.'.format(pack, item['name']))
+                    if not 'n' in input('Dependency installation failed, try to fix? [Y/n]: ').lower():
+                        if not c_logger(stringify=lambda: fix_cmd,
+                                        run=lambda: Popen(fix_cmd).wait(), 0):
+                            raise Exception('Failed to fix dependencies of {}.'.format(item['name']))
+                    else:
+                        raise Exception('Failed to install dependencies of {}.'.format(item['name']))
         # execute final package installations
         v_logger(h.LOG_MODE.INFO, '{}: Installing all packages..'.format(pack))
         install_params = [install_cmd, dry_cmd if DRY_RUN else ''] + [item['name'] for item in items]
-        if c_logger(Sn(stringify=lambda: ' '.join(install_params),
+        if not c_logger(Sn(stringify=lambda: ' '.join(install_params),
                        run=lambda: Popen(install_params).wait()), 0):
             v_logger(h.LOG_MODE.ERR, '{}: Failed to install packages.'.format(pack))
             raise Exception('Failed to complete installation of {}.'.format(pack))
